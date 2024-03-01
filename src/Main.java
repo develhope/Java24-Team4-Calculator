a5df8cc33164edad00016b2aa5cf1f51bdfedd3d
Davidetat1 <davidetatone92@gmail.com>
2024-03-01 12:00:40 +0100

    Feature_divizioneConResto

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,15 @@ public class Main {
         System.out.println("INIT");
     }
 
+    public static void divizione(double num1, double num2) {
+        if (num2 != 0) {
+            double quoziente = num1 / num2;
+            double resto = num1 % num2;
+            System.out.println("Quoziente: " + quoziente);
+            System.out.println("Resto: " + resto);
+        } else {
+            System.out.println("Divisione per zero!");
+        }
+    }
 }
+
