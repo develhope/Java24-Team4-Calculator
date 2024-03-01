3d3132398c40ea17ddf9070b5dd45d79d53f873e
Davidetat1 <davidetatone92@gmail.com>
2024-03-01 13:29:54 +0100

    Feature_PariODisapri

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("INIT");
-    }
+        Scanner inserisciNumero = new Scanner(System.in);
 
+        System.out.println("Inserisci un numero:");
+        int numero = inserisciNumero.nextInt();
+
+        String tipoNumero = pariDispari(numero);
+        System.out.println("Il numero inserito è " + tipoNumero);
+    }
+    public static String pariDispari(int numero) {
+        if (numero % 2 == 0) {
+            return "Pari";
+        } else {
+            return "Dispari";
+        }
+    }
 }
