cc0805961b7f9c78a98ef313b66e717ed09a96ad
Sem Koksharov <samsungj73965@gmail.com>
2024-03-06 13:23:11 +0100

    Deleted : moltiplicazione.java potenza.java

--- src/moltiplicazione.java
+++
@@ -1,7 +0,0 @@
-public class moltiplicazione {
-
-    public static double moltiplica(int num1, int num2) {
-        return num1 * num2;
-    }
-
-}
--- src/potenza.java
+++
@@ -1,5 +0,0 @@
-public class potenza {
-    public static double calcolaPotenza(int num1, int num2) {
-        return Math.pow(num1, num2);
-    }
-}
